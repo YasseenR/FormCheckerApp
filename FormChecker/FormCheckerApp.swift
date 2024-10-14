5bc60a1814d23f15d95dfdc4cf6c7ca64c2deeea
YasseenR <yasseenrouni@gmail.com>
2024-10-14 11:42:53 -0400

    Initial Commit

---
+++ FormChecker/FormCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormCheckerApp.swift
+//  FormChecker
+//
+//  Created by Yasseen Rouni on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FormCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FormCheckerTests/FormCheckerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FormCheckerTests.swift
+//  FormCheckerTests
+//
+//  Created by Yasseen Rouni on 10/14/24.
+//
+
+import Testing
+@testable import FormChecker
+
+struct FormCheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
